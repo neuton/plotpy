29ded050f149c56e7f0e84d53069626f7b8e5aaf
Oleksii Gryniuk <mr.neuton@gmail.com>
2014-11-07 03:15:24 +0100

    major refactoring sketch

--- plot.py
+++ plot.py
@@ -3,15 +3,35 @@
 import matplotlib.pyplot as plt
 import sys
 import shlex
+import getopt
 
+
+class Data:
+	pass
 
 class Plot:
 	
-	def __init__(self, iy, ix=None, iex=None, iey=None, color=None, mrkr=None, lnwd=None, lnstyle=None, label=None):
+	def __init__(self, figure, iy, ix=None, iex=None, iey=None, source=None, kwargs={}):
+		#label = label or source
 		self.kwargs = {'linewidth':lnwd or 1, 'linestyle':lnstyle or '-', 'marker':mrkr, 'label':label}
 		if color is not None:
 			self.kwargs['color'] = color
 		self.ix, self.iy, self.iex, self.iey = ix, iy, iex, iey
+		shift = 0
+		if source is not None:
+			if source in _opened_files:
+				raise Exception('Recursive inclusion!')
+			if source in included_files:
+				shift = sum(map(len, inc_data[:included_files.index(source)]))
+			else:
+				nd, np = parseFile(source)
+				shift = sum(map(len, inc_data))
+				for p in np:
+					p.shift(shift)
+				inc_data.append(nd)
+				inc_plots += np
+				included_files.append(source)
+		self.shift(shift)
 	
 	def shift(self, shift):
 		self.ix, self.iy, self.iex, self.iey = map(lambda i: i if i is None else i+shift, (self.ix, self.iy, self.iex, self.iey))
@@ -41,21 +61,34 @@ class Plot:
 
 
 _default_axis = [None]*4
+_included_files = []
 _opened_files = set()
+
+def isNumber(a):
+	try:
+		float(a)
+	except ValueError:
+		return False
+	else:
+		return True
+
+def parsePlot(s):
+	l = shlex.split(s)
+	if isNumber(l[1]):
+		ix, iy = int(l[0]), int(l[1])
+		l = l[2:]
+	else:
+		ix, iy = None, int(l[0])
+		l = l[1:]
+	kwargs0 = {'marker':None, 'linewidth':1}
+	kwargs = dict(a.split('=') for a in l)
+	kwargs = {k: kwargs.get(k) or kwargs0.get(k) for k in set(kwargs0).union(kwargs)}
+	return iy, ix, kwargs
 
 def parseFile(filename):
 	
-	def isNumber(a):
-		try:
-			float(a)
-		except ValueError:
-			return False
-		else:
-			return True
-	
-	global _default_axis
-	default_style = {'color':None, 'mrkr':None, 'lnstyle':None, 'lnwd':None}
-	included_files, data, inc_data, plots, inc_plots = [], [], [], [], []
+	global _default_axis, _included_files
+	data, inc_data, plots, inc_plots = [], [], [], []
 	
 	global _opened_files
 	_opened_files.add(filename)
@@ -97,54 +130,7 @@ def parseFile(filename):
 			elif a == 'linestyle':
 				default_style['lnstyle'] = val
 			elif a == 'plot':
-				l = shlex.split(val)
-				if isNumber(l[1]):
-					ix, iy = int(l[0]), int(l[1])
-					l = l[2:]
-				else:
-					ix, iy = None, int(l[0])
-					l = l[1:]
-				color = default_style['color']
-				mrkr = default_style['mrkr']
-				lnwd = default_style['lnwd']
-				lnstyle = default_style['lnstyle']
-				iex = iey = label = source = None
-				for s in l:
-					a = s.split('=')
-					pn = a[0].strip()
-					if 'xerr' in pn:
-						iex = int(a[1])
-					elif 'yerr' in pn:
-						iey = int(a[1])
-					elif pn == 'color':
-						color = a[1].strip()
-					elif pn == 'marker':
-						mrkr = a[1].strip()
-					elif pn == 'linewidth':
-						lnwd = float(a[1])
-					elif pn == 'linestyle':
-						lnstyle = a[1]
-					elif pn == 'label':
-						label = a[1]
-					elif pn == 'source':
-						source = a[1]
-				shift = 0
-				label = label or source
-				if source is not None:
-					if source in _opened_files:
-						raise Exception('Recursive inclusion!')
-					if source in included_files:
-						shift = sum(map(len, inc_data[:included_files.index(source)]))
-					else:
-						nd, np = parseFile(source)
-						shift = sum(map(len, inc_data))
-						for p in np:
-							p.shift(shift)
-						inc_data.append(nd)
-						inc_plots += np
-						included_files.append(source)
-				plots.append(Plot(iy,ix,iex,iey,color,mrkr,lnwd,lnstyle,label))
-				plots[-1].shift(shift)
+				plots.append(Plot(parsePlot(val)))
 			elif a == 'include':
 				for f in shlex.split(val):
 					if f in _opened_files:
@@ -173,63 +159,67 @@ def parseFile(filename):
 	return data + sum(inc_data,[]), plots + inc_plots
 
 
-def initFig():
-	plt.gcf().patch.set_facecolor('white')
-	#plt.grid(True, which="both", ls="-", color='gray')
-	plt.minorticks_on()
-	#plt.gca().tick_params(which='minor', bottom='on', labelbottom='on')
-	#plt.setp(plt.gca().get_xticklabels(minor=True), visible=True)
-
-def saveFig(filename):
-	fig = plt.gcf()
-	fig.set_size_inches(18,10)
-	fig.savefig(filename)
-
-def showFig():
-	plt.show()
+class Figure:
+	
+	def __init__(self, *filenames):
+		plt.gcf().patch.set_facecolor('white')
+		#plt.grid(True, which="both", ls="-", color='gray')
+		plt.minorticks_on()
+		#plt.gca().tick_params(which='minor', bottom='on', labelbottom='on')
+		#plt.setp(plt.gca().get_xticklabels(minor=True), visible=True)
+		self.data = []
+		self.plots = []
+		self.load(*filenames)
+	
+	def load(self, *filenames):
+		for f in filenames:
+			data, plots = parse_file(f)
+			for plot in plots:
+				plot.shift(len(self.data))
+			self.data += data
+			self.plots += plots
+	
+	def add_plots(self, *plots):
+		self.plots += plots
+	
+	def draw(self):
+		if self.plots == []:
+			self.plots = [Plot(self, i+1) for i in range(len(self.data))]
+		x1, x2, y1, y2 = _default_axis
+		all_x = sum([[d for d in data[plot.ix-1] if d is not None] if plot.ix is not None else range(len(data[plot.iy-1])) for plot in plots],[])
+		all_y = sum([[d for d in data[plot.iy-1] if d is not None] for plot in plots],[])
+		x2 = x2 or max(all_x)
+		x1 = x1 or min(all_x)
+		y2 = y2 or max(all_y)
+		y1 = y1 or min(all_y)
+		xs = x2 - x1
+		ys = y2 - y1
+		plt.axis([x1-xs/20., x2+xs/20., y1-ys/20., y2+ys/20.])
+		for plot in plots:
+			plot.plot()
+	
+	def show(self):
+		plt.show()
+	
+	def save(self, filename):
+		fig = plt.gcf()
+		fig.set_size_inches(18,10)
+		fig.savefig(filename)
 
 
 if __name__ == '__main__':
 	
-	initFig()
-	
-	args = sys.argv[1:]
-	if '-o' in args:
-		i = args.index('-o')
-		output_filename = args[i+1]
-		filenames = set(args[:i]+args[i+2:])
-	else:
-		output_filename = None
-		filenames = set(args)
-	if filenames == set():
+	args, filenames = getopt.getopt(sys.argv[1:], 'p:o:')
+	if filenames == []:
 		filenames = ['plot.txt']
 	
-	data, plots = [], []
-	for f in filenames:
-		ndata, nplots = parseFile(f)
-		for plot in nplots:
-			plot.shift(len(data))
-		data += ndata
-		plots += nplots
+	figure = Figure(*filenames)
+	figure.add_plots(*[Plot(figure, parse_plot(s)) for o, s in args if o == '-p'])
+	figure.draw()
 	
-	if plots == []:
-		plots = [Plot(i+1) for i in range(len(data))]
-	
-	x1, x2, y1, y2 = _default_axis
-	all_x = sum([[d for d in data[plot.ix-1] if d is not None] if plot.ix is not None else range(len(data[plot.iy-1])) for plot in plots],[])
-	all_y = sum([[d for d in data[plot.iy-1] if d is not None] for plot in plots],[])
-	x2 = x2 or max(all_x)
-	x1 = x1 or min(all_x)
-	y2 = y2 or max(all_y)
-	y1 = y1 or min(all_y)
-	xs = x2 - x1
-	ys = y2 - y1
-	plt.axis([x1-xs/20., x2+xs/20., y1-ys/20., y2+ys/20.])
-	
-	for plot in plots:
-		plot.plot(data)
-	
-	if output_filename is None:
-		showFig()
+	if '-o' not in zip(*args)[0]:
+		figure.show()
 	else:
-		saveFig(output_filename)
+		for o, f in args:
+			if o == '-o':
+				figure.save(f)
